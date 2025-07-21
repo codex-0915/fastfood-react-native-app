bf12abdab4b84800e4d9c88c96ce871726427f0e
Kyl Justin Perez <perezkyljustin@gmail.com>
2025-07-21 20:44:28 +0800

    Add usable custom styles and fonts

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-100">
-      <Text className="text-xl font-bold text-primary">
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
         Welcome to React Native!
       </Text>
     </View>
